c5945366dea4525df7f5f1c9ff5532bea57af7fb
Gezahegn Tsegaye <gezut38@gmail.com>
2025-03-16 23:35:46 +0100

    modified user service

--- user-service/src/main/java/com/egov/tendering/user/config/KafkaConfig.java
+++ user-service/src/main/java/com/egov/tendering/user/config/KafkaConfig.java
@@ -3,6 +3,7 @@ package com.egov.tendering.user.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
@@ -14,9 +15,9 @@ public class KafkaConfig {
     private String userEventsTopic;
 
     @Bean
+    @ConditionalOnProperty(name = "spring.kafka.enabled", havingValue = "true", matchIfMissing = true)
     public NewTopic userEventsTopic() {
-        return TopicBuilder
-                .name(userEventsTopic)
+        return TopicBuilder.name("user-events")
                 .partitions(3)
                 .replicas(1)
                 .build();
